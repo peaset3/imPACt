df49c49a1100717a0ab5c0029181efc898a588a6
planteater8 <bimster9@gmail.com>
2019-11-04 14:42:42 -0600

    Add project files.

---
+++ imPACt/imPACt/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using imPACt.Models;
+
+namespace imPACt.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
