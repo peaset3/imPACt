4d9c193e0039b689eae411cccc9fb4040894d923
jchi4 <42755744+jchi4@users.noreply.github.com>
2019-12-03 14:10:27 -0600

    Update Conversation

--- imPACt/imPACt.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ imPACt/imPACt.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"6b3ab93a-1b3c-415d-a9b7-f4a6a3547b8d"
+	.asciz	"66cfd168-9ead-4a18-885c-daa9035cf50a"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
